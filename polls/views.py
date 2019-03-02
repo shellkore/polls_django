bfd25e86ce9d6f54b6ba9aa64153b046994fba1d
Shailesh Kumar Sahu <shellkoresahu@gmail.com>
2019-03-03 02:17:33 +0530

    removed future questions from view

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from .models import Question,Choice
 from django.http import HttpResponse,HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 #from django.template import loader
 
 '''
@@ -19,7 +20,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 '''def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
