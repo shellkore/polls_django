c7df1dafe7c3de2d0176306eaae95a6c04d7c969
Shailesh Kumar Sahu <shellkoresahu@gmail.com>
2019-03-06 23:53:41 +0530

    added fieldsets in admin view

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+
 from .models import Question,Choice
-# Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
