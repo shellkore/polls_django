b37e2e49a08a8cc032db967d83d7e3826ea15219
Shailesh Kumar Sahu <shellkoresahu@gmail.com>
2019-03-08 03:10:09 +0530

    added filter,search in admin.py

--- polls/models.py
+++ polls/models.py
@@ -10,6 +10,9 @@ class Question(models.Model):
     	return self.question_text
     def was_published_recently(self):
     	return timezone.now()-datetime.timedelta(days=1)<=self.pub_date <=timezone.now()
+    was_published_recently.admin_order_field='pub_date'
+    was_published_recently.boolean=True
+    was_published_recently.short_description='published recently?'
 
 
 
