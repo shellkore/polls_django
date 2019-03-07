f2affb997d610301393aa8fed83d462656307178
Shailesh Kumar Sahu <shellkoresahu@gmail.com>
2019-03-08 00:12:45 +0530

    changed choice view to tabular

--- polls/admin.py
+++ polls/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 
 from .models import Question,Choice
 
+#class ChoiceInLine(admin.StackedInline):
+class ChoiceInLine(admin.TabularInline):
+	model = Choice
+	extra = 2
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
+    inlines= [ChoiceInLine]
 
 admin.site.register(Question, QuestionAdmin)
